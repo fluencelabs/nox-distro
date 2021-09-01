c7f189745fad1fe348e8355913784de4edd157de
folex <0xdxdy@gmail.com>
2021-09-01 09:26:10 +0300

    move services.json to fluence/

---
+++ fluence/download_fluence.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -o pipefail -o errexit -o nounset
+
+if [ "$#" -ne 1 ]; then
+    echo "$0 expects a single argument: URL of fluence.json" >&1
+    exit 1
+fi
+
+echo "*** download fluence.json ***"
+curl -sL "$1" -o fluence.json
+
+jq -r '.url, .sha256, .version' fluence.json |
+    while
+        IFS=''
+        read -r url
+        read -r sha256
+        read -r version
+    do
+        echo "*** download $version ***"
+        # TODO: use --fail-with-body
+        curl -sL --fail $url -o /fluence || (
+            echo "failed to download $url" >&2
+            exit 1
+        )
+        echo "$sha256 /fluence" | sha256sum --check --status || (
+            echo "incorrect SHA256" >&2
+            exit 1
+        )
+        chmod +x /fluence
+    done

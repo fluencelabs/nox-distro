f9bf8c33ef516072339585d319862d3e39679fa1
Anatoly Laskaris <github_me@nahsi.dev>
2022-06-20 16:38:28 +0300

    Cleanup

--- fluence/deploy/entrypoint.sh
+++ fluence/deploy/entrypoint.sh
@@ -1,23 +1,12 @@
 #! /usr/bin/env sh
 
-cleanup() {
-  kill -9 $SSH_AGENT_PID
-  echo "ssh-agent with pid $SSH_AGENT_PID is killed"
-  exit
-}
-
 if ! [[ -d /root/.ssh ]]; then
   echo "Private key at '/root/.ssh' is missing or not readable"
   echo "Did you forget to mount .ssh directory with '-v $HOME/.ssh:/root/.ssh:ro'?"
   exit 1
 fi
 
-# start ssh-agent
 eval $(ssh-agent) > /dev/null
-echo "ssh-agent started with pid $SSH_AGENT_PID"
-# on exit kill ssh-agent
-trap 'cleanup' EXIT INT TERM
-echo "Adding private key to ssh-agent"
 ssh-add
 
 exec fab $@
